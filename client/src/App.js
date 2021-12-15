c7d81bdc529a6dc3423752a3e3b22ae9d32223d2
AbdullahAhmed11 <abdullh442ahmed2@gmail.com>
2021-12-15 13:04:43 +0200

    products-details

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 // import { words } from "./words";
 import data from "./data.json"
 
-
 function App() {
   const [products, setProducts] = useState(data)
-
   return (
     <div className="layout">
       <Header/>
--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../css/Products/Products.css";
+import ProductModal from './ProductModal';
+
 function Products(props) {
-    console.log(props)
+    const [product, setProduct] = useState("")
+
+    const openModal = (product) => {
+        setProduct(product)
+    }
+    const closeModal  = () => {
+        setProduct(false)
+    }
     return (
         <div className="productsEl">
             {props.products.map(product => (
             <div className="product-item" key={product.id}>
+                <a href="#" onClick={() => openModal(product)}>
                     <img src={product.imageUrl} alt={product.title}/>
+                </a>
                     <div className="product-desc">
                         <p>{product.description}</p>
-                        <span>{product.price}</span>
+                        <span>${product.price}</span>
                     </div>
                     <button>Add to cart</button>
             </div>
         ))}
+            <ProductModal product={product} openModal={openModal} closeModal={closeModal}/>
         </div>
         
     )
