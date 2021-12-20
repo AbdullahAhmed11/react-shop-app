da1df97057a138a3fa48e96fb79d64327b08e18b
AbdullahAhmed11 <abdullh442ahmed2@gmail.com>
2021-12-20 09:56:02 +0200

    order-back

--- client/src/components/Cart/orderModal.js
+++ client/src/components/Cart/orderModal.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import "../../css/Cart/Cart.css";
+import Modal from "react-modal"
 
-function orderModal() {
+function orderModal(props) {
+    const {order, closeModal, cartItems} = props
     return (
-        <div>
-            
-        </div>
+        <Modal isOpen={order}>
+            <div class="order-info">
+                <span className='close-model' onClick={closeModal}>&times;</span>
+                <p className="order-success">order done success! congratulations {order.name}</p>
+                <table>
+                    <tr>
+                        <td>Name: </td>
+                        <td>{order.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Email: </td>
+                        <td>{order.email}</td>
+                    </tr>
+                    <tr>
+                        <td>Total: </td>
+                        <td>{cartItems.reduce( (a, p) => {
+                            return a + p.price * p.qty
+                        },0)}</td>
+                    </tr>
+                </table>
+            </div>
+        </Modal>
     )
 }
 export default orderModal
